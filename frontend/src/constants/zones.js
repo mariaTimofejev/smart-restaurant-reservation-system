ff80d7201860f561762e2fc305acb6e202e53da6
Maria Timofejev <maria.timofejev.001@mail.ee>
2026-03-18 17:02:45 +0200

    feat: add frontend zone constants and map enum values to labels

---
+++ frontend/src/constants/zones.js
@@ -0,0 +1,15 @@
+import { ZONES } from "../constants/zones";
+
+export const ZONES = [
+  { value: "TERRACE", label: "Terrace" },
+  { value: "MAIN_HALL", label: "Main Hall" },
+  { value: "PRIVATE_ROOM", label: "Private Room" }
+];
+
+<select value={zone} onChange={(e) => setZone(e.target.value)}>
+  {ZONES.map((z) => (
+    <option key={z.value} value={z.value}>
+      {z.label}
+    </option>
+  ))}
+</select>
